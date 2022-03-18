a1994b4c515bbdbe3e139bb0099b21cc1307f628
allenstr90 <allenstr90@gmail.com>
2022-03-18 11:09:11 -0400

    add default greeting, improve test and refactor

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -2,10 +2,25 @@ package main
 
 import "testing"
 
-func TestHello(t *testing.T){
-	got := Hello("paco")
-	want := "hello paco !!!"
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+func TestHello(t *testing.T) {
+	assertCorrectMesg := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("paco")
+		want := "hello paco !!!"
+
+		assertCorrectMesg(t, got, want)
+	})
+
+	t.Run("saying hello world when empty name", func(t *testing.T) {
+		got := Hello("")
+		want := "hello world !!!"
+
+		assertCorrectMesg(t, got, want)
+	})
 }
